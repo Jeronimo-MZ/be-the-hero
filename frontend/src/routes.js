8bbe11738f2f675d206078cf5ce22995fbb6a52c
Jerónimo Matavel <mataveljeronimo@gmail.com>
2021-03-20 02:21:54 +0200

    add Profile pages

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Logon from './pages/Logon';
+import Profile from './pages/Profile';
 import Register from './pages/Register';
 
 
@@ -9,6 +10,7 @@ const Routes = () => {
         <BrowserRouter>
             <Route path="/" exact component={Logon}/>
             <Route path="/register" component={Register}/>
+            <Route path="/profile" component={Profile}/>
         </BrowserRouter>
     );
 }
